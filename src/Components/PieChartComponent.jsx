1134fc423a47ceac42b53b16a0989c4763e368c4
Tushar singh <110289370+TUSHARSINGHTHEPROGRAMMER@users.noreply.github.com>
2024-09-07 00:49:34 +0530

    Update PieChartComponent.jsx
    pie chart responsive

--- src/Components/PieChartComponent.jsx
+++ src/Components/PieChartComponent.jsx
@@ -1,37 +1,39 @@
 
-import { PieChart, Pie, Tooltip, Cell } from 'recharts';
+import { PieChart, Pie, Tooltip, Cell ,ResponsiveContainer} from 'recharts';
 import PropTypes from 'prop-types';
 
-const PieChartComponent = ({ data }) => {
+const COLORS = ['#82ca9d', '#8884d8', '#FF8042', '#FFBB28','#DA2129']; // Example colors
 
-  const COLORS = ['#FF6384', '#36A2EB', '#FFCE56', '#4CAF50', '#FF9F40'];
-
-  return (
-    <div className="flex justify-center items-center p-8 bg-white rounded-lg shadow-lg">
-      <div>
-        <h2 className="text-2xl font-semibold mb-4 text-center">Revenue Breakdown</h2>
-        <PieChart width={450} height={450}>
-          <Pie
-            data={data}
-            dataKey="value"
-            nameKey="name"
-            cx="50%"
-            cy="50%"
-            outerRadius={150}
-            innerRadius={70}
-            fill="#82ca9d"
-            label
-          >
-            {data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-            ))}
-          </Pie>
-          <Tooltip />
-        </PieChart>
+const PieChartComponent = ({ data }) => (
+  <div className="flex justify-center items-center p-6 bg-white rounded-lg shadow-md">
+    <div className="w-full">
+      <h2 className="text-2xl font-semibold mb-4 text-center">Product Distribution</h2>
+      <div style={{ width: '100%', height: 450 }}>
+        <ResponsiveContainer>
+          <PieChart>
+            <Pie
+              data={data}
+              dataKey="value"
+              nameKey="name"
+              cx="50%"
+              cy="50%"
+              outerRadius="80%"
+              innerRadius="0%"
+              fill="#82ca9d"
+              label
+            >
+              {data.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+              ))}
+            </Pie>
+            <Tooltip />
+          </PieChart>
+        </ResponsiveContainer>
       </div>
     </div>
+  </div>
   );
-};
+
 
 PieChartComponent.propTypes = {
   data: PropTypes.arrayOf(
