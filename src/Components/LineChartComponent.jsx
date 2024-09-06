c1e57736d53900aabc53c5a34e50760116b5b7e7
Tushar singh <110289370+TUSHARSINGHTHEPROGRAMMER@users.noreply.github.com>
2024-09-07 00:49:04 +0530

    Update LineChartComponent.jsx
    responsive lien chart

--- src/Components/LineChartComponent.jsx
+++ src/Components/LineChartComponent.jsx
@@ -1,18 +1,22 @@
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer  } from 'recharts';
 import PropTypes from 'prop-types';
 
 const LineChartComponent = ({ data }) => (
   <div className="flex justify-center items-center p-6 bg-white rounded-lg shadow-md">
-    <div>
+    <div className="w-full">
       <h2 className="text-2xl font-semibold mb-4 text-center">Sales Over Time</h2>
-      <LineChart width={600} height={400} data={data}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="value" stroke="#4F46E5" strokeWidth={2} />
-      </LineChart>
+      <div style={{ width: '100%', height: 400 }}>
+        <ResponsiveContainer>
+          <LineChart data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="value" stroke="#4F46E5" strokeWidth={2} />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   </div>
 );
