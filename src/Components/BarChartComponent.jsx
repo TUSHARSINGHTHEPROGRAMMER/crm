35c796040068bf0a80dca7ce30bd45d2d1e7b663
Tushar singh <110289370+TUSHARSINGHTHEPROGRAMMER@users.noreply.github.com>
2024-09-07 00:48:27 +0530

    Update BarChartComponent.jsx
    responsive bar chart

--- src/Components/BarChartComponent.jsx
+++ src/Components/BarChartComponent.jsx
@@ -1,18 +1,22 @@
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend ,ResponsiveContainer} from 'recharts';
 import PropTypes from 'prop-types';
 
 const BarChartComponent = ({ data }) => (
   <div className="flex justify-center items-center p-6 bg-white rounded-lg shadow-md">
-    <div>
+    <div className="w-full">
       <h2 className="text-2xl font-semibold mb-4 text-center">Product Sales</h2>
-      <BarChart width={600} height={400} data={data}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="value" fill="#6366F1" />
-      </BarChart>
+      <div style={{ width: '100%', height: 400 }}>
+        <ResponsiveContainer>
+          <BarChart data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="value" fill="#6366F1" />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   </div>
 );
